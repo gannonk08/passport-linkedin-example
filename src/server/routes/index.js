08351868453cf47daac9051048a5389d56315226
Kristjan Gannon <gannonk08@gmail.com>
2016-11-30 15:44:20 -0700

    set up passport

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
 router.get('/', home)
 
 function home (req, res, next) {
   res.render('index', { title: 'Welcome to Express!' })
 }
+
+router.get('/linkedin',
+  passport.authenticate('linkedin', { state: 'SOME STATE' }))
+
+router.get('/linkedin/callback', passport.authenticate('linkedin', {
+  successRedirect: '/',
+  failureRedirect: '/'
+}))
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
 
 module.exports = router
